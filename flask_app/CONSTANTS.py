9e353914896106036dab0ad359eb2e93ea2ac59c
lucas <lucasconnell9@gmail.com>
2024-12-13 16:31:13 -0500

    refactor + add constants.py

---
+++ flask_app/CONSTANTS.py
@@ -0,0 +1,4 @@
+UPLOAD_FOLDER = './testData'
+QUESTION =("curate a list from 1 to 5 of 5 cold weather items from my inventory to post on my instagram in a "
+           "vintage pitt winter clothing drop announcement. Also tell me how to schedule the daily posts i.e. time of "
+           "day, what days of the week, and post order for maximum post engagement")
--- flask_app/app.py
+++ flask_app/app.py
@@ -1,31 +1,27 @@
 import logging
 import os
 
-from flask import Flask, jsonify, request, session
+from flask import Flask, jsonify, request
 from werkzeug.utils import secure_filename
 
+from flask_app import CONSTANTS
 from flask_app.aiEngine import AIEngine
 
-UPLOAD_FOLDER = './testData'
-ALLOWED_EXTENSIONS = {'txt'}
-QUESTION =("curate a list from 1 to 5 of 5 cold weather items from my inventory to post on my instagram in a "
-           "vintage pitt winter clothing drop announcement. Also tell me how to schedule the daily posts i.e. time of "
-           "day, what days of the week, and post order for maximum post engagement")
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = CONSTANTS.UPLOAD_FOLDER
 logger = logging.getLogger(__name__)
 
 try:
     ai_engine = AIEngine()
     ai_engine.compile_model()
-except Exception as e:
-    logger.error(e)
+except Exception as ex:
+    logger.error(ex)
 
 @app.route('/api/load_data', methods=['POST'])
 def load_data():
-    target = UPLOAD_FOLDER
-    if not os.path.isdir(UPLOAD_FOLDER):
-        os.mkdir(UPLOAD_FOLDER)
+    target = CONSTANTS.UPLOAD_FOLDER
+    if not os.path.isdir(CONSTANTS.UPLOAD_FOLDER):
+        os.mkdir(CONSTANTS.UPLOAD_FOLDER)
 
     file = request.files['file']
     filename = secure_filename(file.filename)
@@ -44,7 +40,7 @@ def load_data():
 
 @app.route('/api/llm/response', methods=['GET'])
 def llm_response():
-    answer = ai_engine.invoke_model(QUESTION)
+    answer = ai_engine.invoke_model(CONSTANTS.QUESTION)
     return jsonify(answer)
 
 
